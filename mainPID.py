045ea79395216889ba65eb02097d890fe7aa3838
Ubuntu <ubuntu@ip-172-31-86-127.ec2.internal>
2020-11-08 21:20:48 +0000

    commit

--- mainPID.py
+++ mainPID.py
@@ -107,12 +107,12 @@ def timerCallBack(event):
             control = -1
         
         
-        if cont> 5:    
+        if cont> 7:    
             msg.linear.x = control
             msg.angular.z = 0
         else:
-            msg.linear.x = control
-            msg.angular.z = -0.3
+            msg.linear.x = 0
+            msg.angular.z = -0.1
             cont +=1
 
         if abs(error) < 0.05 and abs(sumError) <0.1 and abs(varError) <0.01:
